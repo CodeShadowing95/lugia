b9e48e2199a486783d8d83fcf292ad4d1ee389fc
Frank Patrick Namegni <CodeShadowing95>
2023-03-14 01:10:27 +0100

    Update index.js, App.js and installing some dependencies for package.json

---
+++ lugia_frontend/src/App.js
@@ -0,0 +1,23 @@
+// To install additional dependencies:
+// npm install @sanity/client
+// -||- @sanity/image-url
+// -||- react-google-login
+// -||- react-icons
+// -||- react-loader-spinner
+// -||- react-masonry-css
+// -||- react-router-dom
+// -||- uuid
+
+import React from 'react';
+
+import './index.css';
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App;
