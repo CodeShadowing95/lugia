c548a7eee76302dd45a7b804c772833e126dac03
Frank Patrick Namegni <CodeShadowing95>
2023-03-25 06:49:01 +0100

    Update JSON packages, data, fetchUser, Pin and Home files

--- lugia_frontend/src/components/CreatePin.jsx
+++ lugia_frontend/src/components/CreatePin.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-const CreatePin = () => {
+import { client } from '../client';
+import Spinner from './Spinner';
+import { categories } from '../utils/data';
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [imageAsset, setImageAsset] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
   return (
-    <div>CreatePin</div>
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">Please fill in all the fields.</p>
+      )}
+      <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full">
+        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
+          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
